1a182ccb1bdbe02e499dede3ac5fb78d47f8f3aa
youarebamboozled <56235097+youarebamboozled@users.noreply.github.com>
2023-03-08 09:03:26 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,14 +3,16 @@ import './style.css'
 import App from './App.vue'
 import {createRouter, createWebHistory} from "vue-router";
 import Login from "./components/Login.vue";
-import Home from "./components/Home.vue";
+import Home from "./components/Dashboard.vue";
+
+export const IP = 'http://localhost:8080'
 
 
 export const router = createRouter({
     history: createWebHistory(),
     routes: [
         {path: '/', component: Home},
-        {path: '/login', component: Login}
+        {path: '/login', component: Login},
     ]
 })
 
